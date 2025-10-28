eb8bd7048f8221a8ea9b7779c6b745c3453fe67b
Pedr05TW3 <pedroeusoudoscosta@gmail.com>
2025-10-28 19:42:35 -0300

    commit inicial

---
+++ script/script.js
@@ -0,0 +1,5 @@
+ document.querySelector('.botao-enviar').addEventListener('click', sendEmail);
+function sendEmail() {
+    const mailtoLink = `mailto:pedroeusoudoscosta@gmail.com?subject=Contato%20-%20Portf%C3%B3lio`;
+    window.location.href = mailtoLink;
+}
