d57b2942a76aa79076a6e65d691756a23894b034
Pedr05TW3 <pedroeusoudoscosta@gmail.com>
2025-10-30 22:37:57 -0300

    Início desenvolvimento página skills

--- script/script.js
+++ script/script.js
@@ -1,4 +1,9 @@
- document.querySelector('.botao-enviar').addEventListener('click', sendEmail);
+ import 'charts.css';
+ 
+document.querySelector('.botao-enviar').addEventListener('click', sendEmail);
+
+
+
 function sendEmail() {
     const mailtoLink = `mailto:pedroeusoudoscosta@gmail.com?subject=Contato%20-%20Portf%C3%B3lio`;
     window.location.href = mailtoLink;
